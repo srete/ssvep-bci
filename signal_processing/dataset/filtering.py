1ba4d94351e119e837f1df5ae32104268b101ef4
Teodora Srećković <teodorasrec@gmail.com>
2023-05-15 13:16:45 +0200

    Sredjivanje plotovanja za dataset iz projekta

--- signal_processing/dataset/filtering.py
+++ signal_processing/dataset/filtering.py
@@ -29,6 +29,8 @@ EEG_CHN = {
     'PO8': 7
 }
 
+EEG_CHN_INV = {v: k for k, v in EEG_CHN.items()}
+
 # 1st participant (S02), frequency conf D
 data_folder = r'signal_processing/dataset/raw/s02/'
 data_path = data_folder + r's02_typeD.pkl'
@@ -41,22 +43,89 @@ fs = 250  #Hz
 t = np.arange(0, tf, 1/fs)  #s
 
 freqs = data['freqs']
+FREQ = {i: freqs[i] for i in range(len(freqs))}
 print('Frequencies: ', freqs)
-eeg = data['data'] 
-eeg = np.transpose(eeg, (2, 3, 0, 1))  # shape = (n_frequencies, n_blocks, n_channels, n_samples)
+data = data['data'] 
+data = np.transpose(data, (2, 3, 0, 1))  # shape = (n_frequencies, n_blocks, n_channels, n_samples)
 
+def filter_data(noisy_data):
+    fs = 200
+    # Notch filter
+    quality_factor = 20
+    notch_freq = 50.0  # Hz
+    noisy_data = remove_dc_offset(noisy_data)
+    data_notch= notch_filter(noisy_data, fs, notch_freq, quality_factor)
+    # Chebyshev bandpass filter
+    low=4.5  # Hz
+    high=20  # Hz
+    order=3
+    rp=0.1
+    data_filtered  = cheby_filter(data_notch, fs, low, high, order, rp, ampl_response=False)
+    return data_filtered
+
+#plt.figure()
+# Notch filter
+quality_factor = 20
+notch_freq = 60.0  # Hz
+noisy_data = np.apply_along_axis(remove_dc_offset, -1, data)
+# plt.subplot(311)
+# plt.plot(t, noisy_data[0, 0, 0, :], label='noisy')
+data_notch = np.apply_along_axis(notch_filter, -1, noisy_data, fs, notch_freq, quality_factor)
+# plt.subplot(312)
+# plt.plot(t, data_notch[0, 0, 0, :], label='notch')
+# Chebyshev bandpass filter
+low=5.75  # Hz
+high=13.15  # Hz
+order=2
+rp=0.3
+data_filtered = np.apply_along_axis(cheby_filter, -1, data_notch, fs, low, high, order, rp)
+#plt.subplot(313)
+#plt.plot(t, data_filtered[0, 0, 0, :], label='filtered')
+#plt.show()
+
+data_fft = np.apply_along_axis(signal_fft, -1, data_filtered, fs)
+fft_fs = data_fft[0, 0, 0, 1, :]
+data_fft = data_fft[:, :, :, 0, :]
+
+print(data_fft.shape)
+#plt.plot(fft_fs, data_fft[-1, 0, data_fft.shape[2]-4, :])
+#plt.show()
+
+# function to plot nth trial, all frequencies, all channels
+def plot_trial(n, data, x_axis, FREQ, eeg_idx):
+    print(data.shape)
+    n_fs, n_trial, n_chn, n_samples = data.shape
+    fig, axs = plt.subplots(n_fs, n_chn, figsize=(15, 15))
+    fig.tight_layout(pad=3.0)
+
+    for fs_cnt in range(n_fs):
+        for chn_cnt in range(n_chn):
+            axs[fs_cnt, chn_cnt].plot(x_axis, data[fs_cnt, n, chn_cnt, :])
+            axs[fs_cnt, chn_cnt].set_title('Freq: ' + str(FREQ[fs_cnt]) + ' Hz, Chn: ' + str(EEG_CHN_INV[eeg_idx[chn_cnt]]))
+            axs[fs_cnt, chn_cnt].grid()
+            # set xlim to 0-30 Hz
+            axs[fs_cnt, chn_cnt].set_xlim([4, 15])
+
+    plt.show()
+
+
+chn_idx = [i for i in range(data_fft.shape[2]-4, data_fft.shape[2])]
+plot_trial(0, data_fft[data_fft.shape[0]-4:, :, data_fft.shape[2]-4:, :], fft_fs, freqs[data_fft.shape[0]-4:], chn_idx)
+
+
+'''
 # first 2 seconds of the first block of the first channel (OZ electrode)
 oz_eeg = remove_dc_offset(eeg[-1, 0, EEG_CHN['OZ'], :])
 #po7_eeg = remove_dc_offset(eeg[0, 0, EEG_CHN['PO7'], :])
 
-'''
+###
 To denoise our signal, we first apply a 60 Hz notch filter to remove the EMG noise. 
 Then, to isolate frequencies of interest, we apply a 2nd order Chebyshev bandpass filter with a ripple 
 of 0.3 dB to the range roughly corresponding to flashing frequencies (5.75-13.15Hz). 
 We tuned hyperparameters to optimize performance. We experimented with signal smoothing and a procedure to reject 
 channels based on an RMS threshold but found no significant performance benefits. In the end, we found only 1 
 channel is sufficient and best for our purposes.
-'''
+###
  
 # Notch filter
 quality_factor = 20
@@ -118,3 +187,4 @@ plt.show()
 # plt.subplots_adjust(hspace=0.5)
 # fig.tight_layout()
 # plt.show()
+'''
