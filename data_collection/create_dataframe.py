abd654937bc2012841777b884ce2bb0fd4caf8ca
Teodora Srećković <teodorasrec@gmail.com>
2023-06-13 10:46:18 +0200

    Sredjen folder recorded_data, sklonjena imena ispitanika itd...

--- data_collection/create_dataframe.py
+++ data_collection/create_dataframe.py
@@ -6,10 +6,10 @@ import glob
 session_name = 'ispitanik'
 date = '2023-05-30'
 
-csv_files = glob.glob(r'data_collection\recorded_data\online_app\{}\{}\data\*.csv'.format(date, session_name))
+csv_files = glob.glob(r'data_collection\recorded_data\{}\{}\data\*.csv'.format(date, session_name))
 print('Number of .csv files: ', len(csv_files))
 
 df = pd.concat([pd.read_csv(f) for f in csv_files], ignore_index=True)
-df.to_csv(r'data_collection\recorded_data\online_app\{}\{}\data_all.csv'.format(date, session_name), index=False)
+df.to_csv(r'data_collection\recorded_data\{}\{}\data.csv'.format(date, session_name), index=False)
 print(df)
 
---
+++ data_collection/recorded_data/README.md
@@ -0,0 +1,6 @@
+### Recorded Data Info
+
+'S01' - ispitanik
+Nacini snimanja
+a - Na ekranu se pojavljuju jedna po jedna frekvencija
+b - Sve frekvencije se prikazuju odjednom, a ispitanik se fokusira na jednu od njih
